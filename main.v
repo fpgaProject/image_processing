0e4dafb8cdf8e08c172df502868409c3bad5a33a
mahdieh <65330992+mahdieh-ahmadi@users.noreply.github.com>
2021-01-11 14:40:26 +0330

    clean extra file

--- main.v
+++ main.v
@@ -23,9 +23,10 @@ module main(
     input [7:0] pixel_in1,
    input [7:0] pixel_in2,
    input [7:0] pixel_in3,
+	input [7:0] filter,
     input clk,
     output reg ValidResult,
-    output reg [7:0] Pixel_address,
+    output reg [18:0] Pixel_address = 0,
     output reg [7:0] pixel_out = 0
     );
 
@@ -53,8 +54,19 @@ module main(
  reg [7:0] getData;
  reg fastClock;
  
+ reg [7:0] flag = 0;
+ reg [23:0] orderdRow1;
+ reg [23:0] orderdRow2;
+ reg [23:0] orderdRow3;
+  reg [23:0] orderdColumn;
  
  always @(posedge clk) begin
+  /*
+ p1 p4 p7
+ p2 p5 p8
+ p3 p6 p9
+ */
+ 
   p1 <= pixelsReady[7:0];
   p2 <= pixelsReady[15:8];
   p3 <= pixelsReady[23:16];
@@ -76,21 +88,136 @@ module main(
    end
   end
   if(init == 1'b1 ) begin
-   if(Dx_reg < 0)
-    Dx <= -Dx_reg;
-   else
-    Dx <= Dx_reg;
-   if(Dy_reg < 0)
-    Dy <= -Dy_reg;
-   else
-    Dy <= Dy_reg;
-    if(Dx + Dy <= 127)
-     pixel_out <= 0;
-    else 
-     pixel_out <= 8'b11111111;
-   ValidResult <= 1'b1;
-   Pixel_address <= counter - 3;
-  end
+	if(filter == 0) begin // sobel filter 
+	////////// absolute value /////////
+		if(Dx_reg < 0)
+		 Dx <= -Dx_reg;
+		else
+		 Dx <= Dx_reg;
+		if(Dy_reg < 0)
+		 Dy <= -Dy_reg;
+		else
+		 Dy <= Dy_reg;
+	///// calcute auyput ////////////
+		 if(Dx + Dy <= 127)
+		  pixel_out <= 0;
+		 else 
+		  pixel_out <= 8'b11111111;
+		ValidResult <= 1'b1;
+		Pixel_address <= counter - 3;
+	  end
+	end
+	
+	if(filter == 1) begin
+	////////////// sort row /////////////////
+	  /////////////row 1///////////////
+		if(p4 < p1 && p4 < p7) begin
+			if(p1 < p7) begin // p4 < p1 < p7
+				orderdRow1 = {p7 , p1 , p4};
+			end
+			else begin// p4 < p7 < p1
+			orderdRow1 = {p1 , p7 , p4};
+			end
+		end
+		else if(p1 < p4 && p1 < p7) begin 
+			if(p4 < p7) begin//p1 < p4 < p7
+				orderdRow1 = {p1 , p4 , p7};
+			end
+			else begin//p1 < p7 < p4
+				orderdRow1 = {p4 , p7 , p1};
+			end
+		end
+		else if(p7 < p1 && p7 < p4) begin
+			if(p1 < p4) begin// p7 < p1 < p4
+				orderdRow1 = {p4 , p1 , p7};
+			end
+			else begin//p7 < p4 < p1
+				orderdRow1 = {p1 , p4 , p7};
+			end
+		end
+		/////////////row 2///////////////
+		if(p5 < p2 && p5 < p8) begin
+			if(p2 < p8) begin // p5 < p2 < p8
+				orderdRow2 = {p8 , p2 , p5};
+			end
+			else begin// p5 < p8 < p2
+				orderdRow2 = {p2 , p8 , p5};
+			end
+		end
+		else if(p2 < p5 && p2 < p8) begin 
+			if(p5 < p8) begin//p2 < p5 < p8
+				orderdRow2 = {p8 , p5 , p2};
+			end
+			else begin//p2 < p8 < p5
+			orderdRow2 = {p5 , p8 , p2};
+			end
+		end
+		else if(p8 < p2 && p8 < p5) begin
+			if(p2 < p5) begin// p8 < p2 < p5
+				orderdRow2 = {p5 , p2 , p8};
+			end
+			else begin//p8 < p5 < p2
+				orderdRow2 = {p2 , p5 , p8};
+			end
+		end
+		/////////////row 3///////////////
+		if(p6 < p3 && p6 < p9) begin
+			if(p3 < p9) begin // p6 < p3 < p9
+				orderdRow3 = {p9 , p3 , p6};
+			end
+			else begin// p6 < p9 < p3
+				orderdRow3 = {p3 , p9 , p6};
+			end
+		end
+		else if(p3 < p6 && p3 < p9) begin 
+			if(p6 < p9) begin//p3 < p6 < p9
+				orderdRow3 = {p9 , p6 , p3};
+			end
+			else begin//p3 < p9 < p6
+				orderdRow3 = {p6 , p9 , p3};
+			end
+		end
+		else if(p9 < p3 && p9 < p6) begin
+			if(p3 < p6) begin// p9 < p3 < p6
+				orderdRow3 = {p6 , p3 , p9};
+			end
+			else begin//p9 < p6 < p3
+				orderdRow3 = {p3 , p6 , p9};
+			end
+		end
+		
+		////////////// sort culumn /////////////////
+		/////////////culumn 2///////////////
+		if(orderdRow1[15:8] < orderdRow2[15:8] && orderdRow1[15:8] < orderdRow3[15:8]) begin
+			if(orderdRow2[15:8] < orderdRow3[15:8]) begin // column1 < column2 < column3
+				orderdColumn = {orderdRow3[15:8] , orderdRow2[15:8] , orderdRow1[15:8] };
+			end
+			else begin//  column1 < column3 < column2
+				orderdColumn = {orderdRow2[15:8], orderdRow3[15:8] , orderdRow1[15:8]};
+			end
+		end
+		else if(orderdRow2[15:8] < orderdRow1[15:8] && orderdRow2[15:8] < orderdRow3[15:8]) begin 
+			if(orderdRow1[15:8] < orderdRow3[15:8]) begin//column2 < column1 < column3
+				orderdColumn = {orderdRow3[15:8] , orderdRow1[15:8] , orderdRow2[15:8]};
+			end
+			else begin//column2 < column3 < column1
+				orderdColumn = {orderdRow1[15:8] , orderdRow3[15:8] , orderdRow2[15:8]};
+			end
+		end
+		else if(orderdRow3[15:8] < orderdRow1[15:8] && orderdRow3[15:8] < orderdRow2[15:8]) begin
+			if(orderdRow1[15:8] < orderdRow2[15:8]) begin// column3 < column1 < column2
+				orderdColumn = {orderdRow2[15:8] , orderdRow1[15:8] ,orderdRow3[15:8]};
+			end
+			else begin//column3 < column2 < column1
+				orderdColumn = {orderdRow1[15:8] , orderdRow2[15:8] ,orderdRow3[15:8]};
+			end
+		end
+		/////////////////////////////
+		pixel_out <= orderdColumn[15:8];
+		Pixel_address <= Pixel_address + 1;
+		ValidResult <= 1;
+		
+	end
 
  end
 
--- test_ImageProcessing.m
+++ test_ImageProcessing.m
@@ -5,21 +5,27 @@ hdl_obj = hdlcosim_main;
 ValidResult = false;
 Pixel_address =  uint32(zeros(480,640));
 pixel_out = uint8(zeros(480,640)); 
+pixel_out2 = uint8(zeros(480,640)); 
+pixelResult = 0;
 
-image = imread('Image1.jpg');
+% image = imread('Image1.jpg');
+image = imread('medianImage.png');
 grayImage = rgb2gray(image);
 finalImage = imresize(grayImage , [480 640]);
-% imshow(finalImage)
+imshow(finalImage)
+counter = 1;
 
-for i=0:479
-    for j=1:639
-           x = uint8(finalImage(i+1:i+3,j));
-           x(1)
-           x(2)
-           x(3)
-        [valid , Pixel_address(i*639+j) , pixel_out(i*639+j)]=step(hdl_obj,true,x(1),x(2),x(3));
+for i=1:478
+    for j=1:640
+         x = uint8(finalImage(i:i+2,j));
+        [valid , Pixel_address(i,j) ,pixelResult]=step(hdl_obj,true,x(1),x(2),x(3),uint8(1)); 
+        if(valid == 1)
+            pixel_out(i,j) = pixelResult; 
+        end
     end
 end
 
  imageReady = reshape(pixel_out,480,640);
  imshow(imageReady)
+ 
+ 
